1aef00b36a24e5227423ea18dbacce1544e4bef3
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2019-12-04 20:43:32 -0800

    remove node_modules.

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,10 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL, 
     burger_name VARCHAR (50) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY(id)
 );
 
